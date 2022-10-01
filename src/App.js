c524fe3cc87712482acec6fc54e3cfcc7e1885d9
Erryn <laughterayoola@gmail.com>
2022-10-01 19:20:31 +0100

    Imported components to app.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
 
 function App() {
   return (
-    <div className="App">
-      <hi className="text-3xl text-green-400">Habinger is an Otto!!!</hi>
+    <div className="text-center bg-red-900 md:bg-slate-600 md">
+      <Navbar />
+      <hi className="text-3xl text-green-400 bg-slate-300">
+        Habinger is an Otto!!!
+      </hi>
+      <Feed />
     </div>
   );
 }
